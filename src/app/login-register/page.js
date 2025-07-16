51dc05114676fb4ad6a581898b64378476647dfb
Mehdi Haghdoost <mahdi.leo2014@gmail.com>
2025-07-16 17:30:07 +0330

    Implement auth step switching in login-register page, add components and styles directories

--- src/app/login-register/page.js
+++ src/app/login-register/page.js
@@ -1,10 +1,42 @@
 "use client";
+import styles from '@/styles/login-register.module.css';
 import { useState } from "react";
-const login_register = () => {
-    return (
-        <div>login_register</div>
-        
 
+import Login from "@/components/templates/login-register/Login";
+import Register from "@/components/templates/login-register/Register";
+import ForgetPassword from '@/components/templates/login-register/ForgetPassword';
+import ResetPassword from '@/components/templates/login-register/ResetPassword ';
+import ResetSuccess from '@/components/templates/login-register/ResetSuccess ';
+
+
+const login_register = () => {
+    const [step, setStep] = useState('login');
+
+    const renderStep = () => {
+        switch (step) {
+            case 'login':
+                return <Login goto={setStep} />;
+
+            case 'register':
+                return <Register goto={setStep} />;
+
+            case 'forget':
+                return <ForgetPassword goto={setStep} />;
+
+            case 'reset':
+                return <ResetPassword goto={setStep} />;
+
+            case 'success':
+                return <ResetSuccess goto={setStep} />;
+            default:
+                return < Login goto={setStep} />;
+        }
+    };
+
+    return (
+        <main>
+            {renderStep()}
+        </main>
     )
 }
 
