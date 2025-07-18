64404c12fc5e9a7eaa695882bf1df84dd4c5f5ca
Mehdi Haghdoost <mahdi.leo2014@gmail.com>
2025-07-18 21:31:10 +0330

    Create AuthCard wrapper component with routing
    - Add switch case for different card types
    - Implement wrapper with shared styling (flex, rounded, bg-white)
    - Route to specific card components (Chart, Testimonial, Bar, Feature, FreeStorage)
    - Add consistent className structure for all cards

--- src/components/modules/login-register/AuthCard.js
+++ src/components/modules/login-register/AuthCard.js
@@ -1,10 +1,78 @@
 import styles from './authCard.module.css';
+import BarCard from './BarCard';
+import ChartCard from './ChartCard';
+import FeatureCard from './FeatureCard';
+import FreeStorageCard from './FreeStorageCard';
+import TestimonialCard from './TestimonialCard';
 
-const AuthCard = ({ children, className = '' }) => {
-    return (
-        <div className={`flex flex-col  rounded-[28px] bg-white ${className}`}>
-            {children}
-        </div>
-    )
+const AuthCard = ({ card }) => {
+   
+    // return (
+    //     <div className={`flex flex-col  rounded-[28px] bg-white `}>
+
+    //     </div>
+    // )
+    switch (card.type) {
+        case 'chart':
+            console.log('Rendering ChartCard');
+            return (
+                <ChartCard
+                    title={card.title}
+                    percentage={card.percentage}
+                    timeframe={card.timeframe}
+                    description={card.description}
+                    image={card.image}
+                    className={card.className}
+                />
+            );
+
+        case 'testimonial':
+            console.log('Rendering TestimonialCard');
+            return (
+                <TestimonialCard
+                    quote={card.quote}
+                    user={card.user}
+                    className={card.className}
+                />
+            );
+
+        case 'bar':
+            console.log('Rendering BarCard');
+            return (
+                <BarCard
+                    title={card.title}
+                    percentage={card.percentage}
+                    timeframe={card.timeframe}
+                    description={card.description}
+                    image={card.image}
+                    className={card.className}
+                />
+            );
+        case 'feature':
+            console.log('Rendering FeatureCard');
+            return (
+                <FeatureCard
+                    title={card.title}
+                    percentage={card.percentage}
+                    timeframe={card.timeframe}
+                    description={card.description}
+                    icon={card.icon}
+                    className={card.className}
+                />
+            );
+        case 'freeStorage':
+            console.log('Rendering FreeStorageCard');
+            return (
+                <FreeStorageCard
+                    title={card.title}
+                    icon={card.icon}
+                    storage={card.storage}
+                    description={card.description}
+                    className={card.className}
+                />
+            );
+        default:
+            return null;
+    }
 }
 export default AuthCard;
