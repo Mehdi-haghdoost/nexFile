18d6c0a97451fe8f51cd65bbc9aac93428e5b213
Mehdi Haghdoost <mahdi.leo2014@gmail.com>
2025-07-18 21:30:59 +0330

    Update AuthLayout to support dynamic cards
    - Add cards prop to receive dynamic card data
    - Pass cards prop to AuthLeftPanel component
    - Maintain layout structure with dynamic content

--- src/components/layouts/Auth/AuthLayout.js
+++ src/components/layouts/Auth/AuthLayout.js
@@ -1,13 +1,15 @@
 import styles from './authLayout.module.css';
 import AuthLeftPanel from './AuthLeftPanel';
 
-const AuthLayout = ({ children }) => {
+const AuthLayout = ({ children,cards }) => {
+
+console.log('AuthLayout Cards ===>', cards);
 
     return (
         <div className='flex w-full h-[1024px] bg-white pt-[12px]'>
             {/* ستون سمت چپ */}
             <div className={`flex flex-1  h-full overflow-hidden`}>
-                <AuthLeftPanel />
+                <AuthLeftPanel cards={cards} />
             </div>
             {/* ستون سمت راست */}
             <div className={`flex-1  ${styles.authLayout_rightSide}`}>
