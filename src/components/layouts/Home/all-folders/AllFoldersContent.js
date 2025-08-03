4cac137870af2c0d4f44715f41a339935a395e34
Mehdi Haghdoost <mahdi.leo2014@gmail.com>
2025-08-03 11:25:02 +0330

    feat: implement AllFoldersContent layout component
    - Created AllFoldersContent as main content container for all folders view
    - Added proper component

--- src/components/layouts/Home/all-folders/AllFoldersContent.js
+++ src/components/layouts/Home/all-folders/AllFoldersContent.js
@@ -2,10 +2,12 @@ import FileSection from '@/components/templates/home/All folder/FileSection'
 import FolderSection from '@/components/templates/home/All folder/FolderSection'
 import SuggestedSection from '@/components/templates/home/All folder/SuggestedSection'
 import React from 'react'
+import ActionButtons from '../ActionButtons'
 
 const AllFoldersContent = () => {
     return (
-        <div>
+        <div className='flex py-6 px-8 flex-col items-start gap-6 flex-1 self-stretch bg-white'>
+            <ActionButtons />
             <FolderSection />
             <SuggestedSection />
             <FileSection />
