5cb687e2139115b05bd01598fe499f000cfa1493
Mehdi Haghdoost <mahdi.leo2014@gmail.com>
2025-08-22 11:00:05 +0330

    update: modify ModalManager.js import path for ShareModal refactoring

--- src/components/layouts/Modal/ModalManager.js
+++ src/components/layouts/Modal/ModalManager.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import CreateFolderModal from '@/components/modules/Modals/CreateFolderModal';
-import ShareModal from '@/components/modules/Modals/ShareModal';
+import ShareModal from '@/components/modules/Modals/ShareModal/ShareModal';
 // import UploadFileModal from './UploadFileModal';
 // import EditDocumentModal from './EditDocumentModal';
 // سایر مدال‌ها را در اینجا import کنید
