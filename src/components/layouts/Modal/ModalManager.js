240fa3b77870cc6abcf41d77bc331c9770d56ec3
Mehdi Haghdoost <mahdi.leo2014@gmail.com>
2025-08-31 16:51:49 +0330

    Update ModalManager imports or structure

--- src/components/layouts/Modal/ModalManager.js
+++ src/components/layouts/Modal/ModalManager.js
@@ -3,6 +3,7 @@ import CreateFolderModal from '@/components/modules/Modals/CreateFolderModal';
 import ShareModal from '@/components/modules/Modals/ShareModal/ShareModal';
 import CreateFileModal from '@/components/modules/Modals/CreateFileModal/CreateFileModal';
 import ShareSettingsModal from '@/components/modules/Modals/shareSettingsModal/ShareSettingsModal';
+import EditPdfModal from '@/components/modules/Modals/editPdfModal/EditPdfModal';
 // import UploadFileModal from './UploadFileModal';
 // import EditDocumentModal from './EditDocumentModal';
 // سایر مدال‌ها را در اینجا import کنید
@@ -14,6 +15,7 @@ const ModalManager = () => {
       <ShareModal />
       <CreateFileModal />
       <ShareSettingsModal />
+      <EditPdfModal />
       {/* سایر مدال‌ها */}
       {/* <UploadFileModal /> */}
       {/* <EditDocumentModal /> */}
