837d402255d86ab405d57e13240c251c41d039c4
Mehdi Haghdoost <mahdi.leo2014@gmail.com>
2025-09-07 09:59:08 +0330

    Refactor PdfEditorLayout: Simplify layout structure and integrate with pdfEditorStore

--- src/components/layouts/pdf-editor/PdfEditorLayout.js
+++ src/components/layouts/pdf-editor/PdfEditorLayout.js
@@ -1,20 +1,25 @@
-"use client";
-
-import PdfEditorHeader from '@/components/templates/pdf-editor/PdfEditorHeader';
-import PdfEditorMainArea from '@/components/templates/pdf-editor/PdfEditorMainArea';
-import PdfEditorSidebar from '@/components/templates/pdf-editor/PdfEditorSidebar';
-import PdfEditorToolbar from '@/components/templates/pdf-editor/PdfEditorToolbar';
+'use client';
 import React from 'react';
+import PdfEditorHeader from '@/components/templates/pdf-editor/PdfEditorHeader';
+import PdfEditorToolbar from '@/components/templates/pdf-editor/PdfEditorToolbar';
+import PdfEditorSidebar from '@/components/templates/pdf-editor/PdfEditorSidebar';
+import PdfEditorMainArea from '@/components/templates/pdf-editor/PdfEditorMainArea';
+import usePdfEditorStore from '@/store/pdfEditorStore';
+
 
 
 const PdfEditorLayout = () => {
+    const { activeEditingTool } = usePdfEditorStore();
+    
+    console.log('PdfEditorLayout - activeEditingTool:', activeEditingTool);
+
     return (
         <div className='flex justify-center items-center min-h-screen bg-gray-50'>
             <div className='flex flex-col w-full h-screen'>
                 <PdfEditorHeader />
                 <PdfEditorToolbar />
 
-                <div className='flex flex-1 '>
+                <div className='flex flex-1'>
                     <PdfEditorSidebar />
                     <PdfEditorMainArea />
                 </div>
