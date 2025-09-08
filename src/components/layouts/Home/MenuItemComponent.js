eba459c846b750ba9bd28b253648714fbccec1c9
Mehdi Haghdoost <mahdi.leo2014@gmail.com>
2025-09-08 10:40:22 +0330

    feat: add MenuItemComponent.js - individual navigation item component

---
+++ src/components/layouts/Home/MenuItemComponent.js
@@ -0,0 +1,20 @@
+// MenuItemComponent.js
+import MenuIcon from './MenuIcon';
+
+const MenuItemComponent = ({ item, isActive, onClick }) => {
+  return (
+    <li 
+      className={`flex items-center h-[38px] py-1 px-3 gap-2 self-stretch cursor-pointer ${isActive ? 'nav-item-active' : ''}`}
+      onClick={() => onClick(item.key)}
+    >
+      <div className='flex justify-center items-center h-4 w-4'>
+        <MenuIcon iconType={item.icon} isActive={isActive} />
+      </div>
+      <h3 className='text-medium-14'>
+        {item.display}
+      </h3>
+    </li>
+  );
+};
+
+export default MenuItemComponent;
