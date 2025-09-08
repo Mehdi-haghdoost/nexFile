f169a997d2b2f9c5f279f48fc4668d781d6c2c57
Mehdi Haghdoost <mahdi.leo2014@gmail.com>
2025-09-08 11:05:41 +0330

    feat: update SignaturesContent.js with signature-specific action buttons

--- src/components/layouts/Home/signatures/SignaturesContent.js
+++ src/components/layouts/Home/signatures/SignaturesContent.js
@@ -1,10 +1,12 @@
 import SignaturesList from '@/components/templates/home/signatures/SignaturesList'
 import StartFromSignature from '@/components/templates/home/signatures/StartFromSignature'
 import React from 'react'
+import ActionButtons from '../ActionButtons'
 
 const SignaturesContent = () => {
     return (
-        <div>
+        <div className='flex py-6 px-8 flex-col items-start gap-6  self-stretch bg-white'>
+             <ActionButtons activeSection="signatures" />
             <StartFromSignature />
             <SignaturesList />
         </div>
