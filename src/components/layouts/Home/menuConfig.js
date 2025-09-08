c9c33d3c9cd12b3468dee185df2d044bece46119
Mehdi Haghdoost <mahdi.leo2014@gmail.com>
2025-09-08 10:40:09 +0330

    feat: add menuConfig.js - navigation menu items configuration

---
+++ src/components/layouts/Home/menuConfig.js
@@ -0,0 +1,38 @@
+// menuConfig.js
+export const MENU_ITEMS = [
+  {
+    key: 'all-folders',
+    display: 'All folder',
+    icon: 'all-folders'
+  },
+  {
+    key: 'signatures',
+    display: 'Signatures',
+    icon: 'signatures'
+  },
+  {
+    key: 'send-and-monitor',
+    display: 'Send and monitor',
+    icon: 'send-and-monitor'
+  },
+  {
+    key: 'shared',
+    display: 'Shared',
+    icon: 'shared'
+  },
+  {
+    key: 'file-requests',
+    display: 'File requests',
+    icon: 'file-requests'
+  },
+  {
+    key: 'deleted-files',
+    display: 'Deleted files',
+    icon: 'deleted-files'
+  },
+  {
+    key: 'admin-console',
+    display: 'Admin console',
+    icon: 'admin-console'
+  }
+];
