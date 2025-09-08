884293462c566f66ff05fdb1f3f7d7b0228831cd
Mehdi Haghdoost <mahdi.leo2014@gmail.com>
2025-09-08 11:05:22 +0330

    refactor: update AllFoldersContent.js to pass activeSection prop to ActionButtons

--- src/components/layouts/Home/all-folders/AllFoldersContent.js
+++ src/components/layouts/Home/all-folders/AllFoldersContent.js
@@ -7,7 +7,7 @@ import ActionButtons from '../ActionButtons'
 const AllFoldersContent = () => {
     return (
         <div className='flex py-6 px-8 flex-col items-start gap-6 flex-1 self-stretch bg-white'>
-            <ActionButtons />
+            <ActionButtons activeSection="all-folders" />
             <FolderSection />
             <SuggestedSection />
             <FileSection />
