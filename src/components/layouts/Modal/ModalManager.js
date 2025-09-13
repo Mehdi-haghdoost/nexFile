b8ebe4429678904609f6c138ff2d1ab9477df7b2
Mehdi Haghdoost <mahdi.leo2014@gmail.com>
2025-09-13 11:43:43 +0330

    feat: Add CreateSignatureModal to ModalManager
    - Import and register CreateSignatureModal component
    - Add modal rendering for createSignature modal type
    - Maintain existing modal management structure

--- src/components/layouts/Modal/ModalManager.js
+++ src/components/layouts/Modal/ModalManager.js
@@ -4,6 +4,8 @@ import ShareModal from '@/components/modules/Modals/ShareModal/ShareModal';
 import CreateFileModal from '@/components/modules/Modals/CreateFileModal/CreateFileModal';
 import ShareSettingsModal from '@/components/modules/Modals/shareSettingsModal/ShareSettingsModal';
 import EditPdfModal from '@/components/modules/Modals/editPdfModal/EditPdfModal';
+import CreateSignatureModal from '@/components/modules/Modals/CreateSignatureModal/CreateSignatureModal';
+
 // import UploadFileModal from './UploadFileModal';
 // import EditDocumentModal from './EditDocumentModal';
 // سایر مدال‌ها را در اینجا import کنید
@@ -16,6 +18,7 @@ const ModalManager = () => {
       <CreateFileModal />
       <ShareSettingsModal />
       <EditPdfModal />
+      <CreateSignatureModal />
       {/* سایر مدال‌ها */}
       {/* <UploadFileModal /> */}
       {/* <EditDocumentModal /> */}
