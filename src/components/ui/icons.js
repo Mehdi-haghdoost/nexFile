5943e375e9a31890db405e023468def880f90591
Mehdi Haghdoost <mahdi.leo2014@gmail.com>
2025-09-21 17:34:41 +0330

    feat: add ViewIcon to icons library
    - Add ViewIcon component for consistent viewing functionality across app

--- src/components/ui/icons.js
+++ src/components/ui/icons.js
@@ -433,6 +433,13 @@ export const ViewerIcon = () => (
     </svg>
 );
 
+export const ViewIcon = () => (
+    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" fill="none">
+        <path d="M6.66667 8C6.66667 8.35362 6.80714 8.69276 7.05719 8.94281C7.30724 9.19286 7.64638 9.33333 8 9.33333C8.35362 9.33333 8.69276 9.19286 8.94281 8.94281C9.19286 8.69276 9.33333 8.35362 9.33333 8C9.33333 7.64638 9.19286 7.30724 8.94281 7.05719C8.69276 6.80714 8.35362 6.66667 8 6.66667C7.64638 6.66667 7.30724 6.80714 7.05719 7.05719C6.80714 7.30724 6.66667 7.64638 6.66667 8Z" stroke="#2E2E37" strokeWidth="1.2" strokeLinecap="round" strokeLinejoin="round" />
+        <path d="M14 8C12.4 10.6667 10.4 12 8 12C5.6 12 3.6 10.6667 2 8C3.6 5.33333 5.6 4 8 4C10.4 4 12.4 5.33333 14 8Z" stroke="#2E2E37" stroke-width="1.2" stroke-linecap="round" stroke-linejoin="round" />
+    </svg>
+);
+
 export const SortIcon = () => (
     <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 18 18" fill="none">
         <path d="M3 4.5H9.75M3 9H8.25M3 13.5H8.25M11.25 11.25L13.5 13.5M13.5 13.5L15.75 11.25M13.5 13.5V4.5" stroke="#737379" strokeWidth="1.2" strokeLinecap="round" strokeLinejoin="round" />
