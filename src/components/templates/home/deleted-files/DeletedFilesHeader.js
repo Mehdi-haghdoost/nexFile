1de8d37e34e2bb406273777e326bd80071b86300
Mehdi Haghdoost <mahdi.leo2014@gmail.com>
2025-09-28 17:50:47 +0330

    feat: add DeletedFilesHeader component for deleted files section

---
+++ src/components/templates/home/deleted-files/DeletedFilesHeader.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const DeletedFilesHeader = () => {
+  return (
+    <header>
+      <h1 className='text-medium-18'>Deleted files</h1>
+    </header>
+  )
+}
+
+export default DeletedFilesHeader
