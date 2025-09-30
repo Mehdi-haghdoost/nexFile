e16a518da9537ca5e69788f71c0eb7ef5f1165bf
Mehdi Haghdoost <mahdi.leo2014@gmail.com>
2025-09-30 11:34:38 +0330

    feat: create folder page with dynamic file display
    - Add /folder route with FolderLayout
    - Integrate ActionButtons, SuggestedSection, and FileSection
    - Display files based on selected folder from sidebar

---
+++ src/app/folder/page.js
@@ -0,0 +1,20 @@
+'use client';
+
+import FolderLayout from '@/components/layouts/Folder/FolderLayout';
+import ActionButtons from '@/components/layouts/Home/ActionButtons';
+import FileSection from '@/components/templates/home/allFolder/FileSection';
+import SuggestedSection from '@/components/templates/home/allFolder/SuggestedSection';
+
+const FolderPage = () => {
+    return (
+        <FolderLayout>
+            <div className='flex py-6 px-8 flex-col items-start gap-6 flex-1 self-stretch bg-white'>
+                <ActionButtons activeSection="all-folders" />
+                <SuggestedSection />
+                <FileSection />
+            </div>
+        </FolderLayout>
+    );
+};
+
+export default FolderPage;
