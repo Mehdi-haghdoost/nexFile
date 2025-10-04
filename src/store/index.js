4bd2b2162e363ea16acee8c00f6f78c8f444cb5c
Mehdi Haghdoost <mahdi.leo2014@gmail.com>
2025-10-04 11:10:21 +0330

    feat: export transferStore in store index

--- src/store/index.js
+++ src/store/index.js
@@ -3,6 +3,7 @@ export { default as useFilesStore } from './features/files/filesStore'
 // export { default as useFoldersStore } from './features/folders/foldersStore'
 export { default as usePdfEditorStore } from './features/pdf-editor/pdfEditorStore'
 // export { default as useSignaturesStore } from './features/signatures/signaturesStore'
+export { default as useTransferStore } from './features/transfer/transferStore'
 
 // UI
 export { default as useModalStore } from './ui/modalStore'
