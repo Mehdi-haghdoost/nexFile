ccaac9dff0a5d952c6a18190754726234502f94c
Mehdi Haghdoost <mahdi.leo2014@gmail.com>
2025-10-27 11:16:00 +0330

    fix: add z-index and relative positioning for dropdown overlap issues

--- src/components/layouts/Home/FileManagementLayout.js
+++ src/components/layouts/Home/FileManagementLayout.js
@@ -3,13 +3,13 @@ import Header from './Header'
 
 const FileManagementLayout = ({ children, onSidebarChange, activeSection }) => {
     return (
-        <div className='flex justify-center items-start w-full min-h-screen bg-white'>
-            <div className='flex w-full max-w-[1440px]'>
+        <div className='flex justify-center items-start w-full min-h-screen bg-white relative'>
+            <div className='flex w-full max-w-[1440px] relative'>
                 <Sidebar
                     onSidebarChange={onSidebarChange}
                     activeSection={activeSection}
                 />
-                <div className='flex flex-1 flex-col items-start flex-shrink-0 border-t border-r border-l border-[#F2F2F3]'>
+                <div className='flex flex-1 flex-col items-start flex-shrink-0 border-t border-r border-l border-[#F2F2F3] relative z-10'>
                     <Header />
                     {children}
                 </div>
