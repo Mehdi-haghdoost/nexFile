6e5e7eaf079e4ab746e419b96880d8cd7940912a
Mehdi Haghdoost <mahdi.leo2014@gmail.com>
2025-10-28 17:26:33 +0330

    feat: add dark mode stroke color for dropdown SVG icon in SidebarHeader

--- src/components/layouts/Home/SidebarHeader.js
+++ src/components/layouts/Home/SidebarHeader.js
@@ -6,10 +6,10 @@ const SidebarHeader = () => {
       <div className={styles.navbar_img}>
       </div>
       <div className='flex flex-col justify-center items-start flex-1'>
-        <p className='text-medium-16'>
+        <p className='text-medium-16 dark:text-medium-16-white'>
           Ridwan T.
         </p>
-        <p className='text-regular-12 '>
+        <p className='text-regular-12 dark:text-regular-12-neutral-200'>
           ridwant@gmail.com
         </p>
       </div>
@@ -17,7 +17,7 @@ const SidebarHeader = () => {
         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" fill="none">
           <path 
             d="M4.66666 9.99996L7.99999 13.3333L11.3333 9.99996M4.66666 5.99996L7.99999 2.66663L11.3333 5.99996" 
-            stroke="#737379" 
+            className="stroke-[#737379] dark:stroke-[#9F9FA3]"
             strokeWidth="1.2"
             strokeLinecap="round" 
             strokeLinejoin="round" 
