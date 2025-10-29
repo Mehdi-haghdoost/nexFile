7b8dbc018be2962809ab2cf777a24f0f62a597be
Mehdi Haghdoost <mahdi.leo2014@gmail.com>
2025-10-29 17:20:16 +0330

    feat: enhance ActionButtonsCard with iconColor prop to support dark mode active state styling

--- src/components/modules/home/ActionButtonsCard.js
+++ src/components/modules/home/ActionButtonsCard.js
@@ -1,7 +1,7 @@
-const ActionButtonsCard = ({ icon, title, bgColor, borderColor, textColor, onClick }) => {
+const ActionButtonsCard = ({ icon, title, bgColor, borderColor, textColor, iconColor, onClick }) => {
     return (
         <div
-            className={`flex flex-col p-3 items-start gap-2 flex-1 rounded-lg border ${borderColor} ${bgColor} cursor-pointer transition-all duration-200 hover:scale-105`}
+            className={`flex flex-col p-3 items-start gap-2 flex-1 rounded-lg border ${borderColor} ${bgColor} ${iconColor} cursor-pointer transition-all duration-200 hover:scale-105`}
             onClick={onClick}
         >
             <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
