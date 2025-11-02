c0f1f6125a9611e743bbddfef1dc42695efe66d9
Mehdi Haghdoost <mahdi.leo2014@gmail.com>
2025-11-02 12:03:34 +0330

    refactor: update AccessControlSection with dark mode compatible icons and improve access control UI consistency

--- src/components/modules/Modals/ShareModal/AccessControlSection.js
+++ src/components/modules/Modals/ShareModal/AccessControlSection.js
@@ -7,8 +7,8 @@ const AccessControlSection = ({ sharedUsers }) => {
             <div className='flex items-center h-[42px] gap-3 self-stretch'>
                 <img src="/images/nav_img.png" className='w-[38px] h-[38px] rounded-[38px]' alt="nav_img.png" />
                 <div className='flex flex-col flex-1 justify-center items-start '>
-                    <h2 className='text-medium-16 '>Ridwan T. (you)</h2>
-                    <span className='text-regular-12'>ridwant@gmail.com</span>
+                    <h2 className='text-medium-16 dark:text-medium-16-white'>Ridwan T. (you)</h2>
+                    <span className='text-regular-12 dark:text-regular-12-neutral-300'>ridwant@gmail.com</span>
                 </div>
                 <p className='text-regular-12-neutral-100'>Owner</p>
             </div>
@@ -27,17 +27,17 @@ const AccessControlSection = ({ sharedUsers }) => {
 
             <div className='flex flex-col items-start gap-2 self-stretch'>
                 <div className='flex justify-between items-center self-stretch'>
-                    <span className='text-regular-12'>Public access</span>
+                    <span className='text-regular-12 dark:text-regular-12-neutral-300'>Public access</span>
                     <div className='flex justify-center items-center gap1.5'>
-                        <h3 className='text-medium-12'>Can view</h3>
+                        <h3 className='text-medium-12 dark:text-medium-12-white pr-1'>Can view</h3>
                         <PermissionsDropdownIcon />
                     </div>
                 </div>
-                <div className='flex justify-center items-center h-[52px] py-3 px-4 gap-2 self-stretch rounded-lg border border-[#E1E0E5] bg-white'>
+                <div className='flex justify-center items-center h-[52px] py-3 px-4 gap-2 self-stretch rounded-lg border border-[#E1E0E5] bg-white dark:bg-neutral-900 dark:border-neutral-600'>
                     <GlobeIcon />
                     <div className='flex flex-col flex-1 justify-center items-start gap-0.5'>
-                        <p className='text-medium-12'>Anyone who has a link</p>
-                        <p className='text-regular-12-neutral-200'>Anyone with a link can see</p>
+                        <p className='text-medium-12 dark:text-medium-12-white'>Anyone who has a link</p>
+                        <p className='text-regular-12-neutral-200 dark:text-regular-12-neutral-200'>Anyone with a link can see</p>
                     </div>
                     <PublicAccessDropdownIcon />
                 </div>
