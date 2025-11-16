cb3e7dddc7866b297753466baa123fd4e30ea4a6
Mehdi Haghdoost <mahdi.leo2014@gmail.com>
2025-11-16 12:51:59 +0330

    fix: set full height with h-screen and add overflow-auto for admin layout

--- src/components/layouts/Admin/AdminLayout.js
+++ src/components/layouts/Admin/AdminLayout.js
@@ -10,7 +10,7 @@ const AdminLayout = ({ children, onSidebarChange }) => {
     const handleCollapseToggle = () => setIsCollapsed(prev => !prev);
 
     return (
-        <div className='flex items-start bg-white min-h-screen w-full'>
+        <div className='flex items-start bg-white h-screen w-full'>
             <AdminSidebar 
                 onBackToNexFile={onSidebarChange} 
                 activeSection={activeSection}
@@ -19,9 +19,9 @@ const AdminLayout = ({ children, onSidebarChange }) => {
                 onCollapseToggle={handleCollapseToggle} 
             />
             
-            <div className='flex flex-1 flex-col items-start flex-shrink-0 w-full min-h-screen border-t border-r border-solid border-[#F2F2F3]'>
+            <div className='flex flex-1 flex-col items-start flex-shrink-0 w-full h-screen border-t border-r border-solid border-[#F2F2F3]'>
                 <AdminHeader activeSection={activeSection} />
-                <main className='flex-1 w-full'>
+                <main className='flex-1 w-full overflow-auto'>
                     {typeof children === 'function' ? children({ activeSection }) : children}
                 </main>
             </div>
