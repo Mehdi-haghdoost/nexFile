bed2929d32cb337e6988d4ceb20189af6a9595c8
Mehdi Haghdoost <mahdi.leo2014@gmail.com>
2025-11-29 10:59:05 +0330

    feat: Add suggested items constants
    - Create SUGGESTED_ITEMS array with sample data
    - Include id, image, title, and time fields
    - Centralize suggested content data
    - Follow project constants pattern

---
+++ src/utils/constants/suggestedConstants.js
@@ -0,0 +1,26 @@
+export const SUGGESTED_ITEMS = [
+  {
+    id: 1,
+    image: '/images/folder.png',
+    title: 'Design Brief DOCX',
+    time: '1h ago'
+  },
+  {
+    id: 2,
+    image: '/images/folder.png',
+    title: 'Wireframe V2 DOCX',
+    time: '2h ago'
+  },
+  {
+    id: 3,
+    image: '/images/folder.png',
+    title: 'User Flow DOCX',
+    time: '3h ago'
+  },
+  {
+    id: 4,
+    image: '/images/folder.png',
+    title: 'Style Guide DOCX',
+    time: '5h ago'
+  }
+];
