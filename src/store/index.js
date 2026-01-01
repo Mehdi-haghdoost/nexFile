41ce01300b9f3c2bf794cba713b5d4ed60389739
Mehdi Haghdoost <mahdi.leo2014@gmail.com>
2026-01-01 12:04:52 +0330

    refactor: update folders store for dynamic data
    - Remove hardcoded folder data
    - Add API integration actions
    - Add folder selection and expansion logic
    - Export foldersStore in store index

--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,11 @@
 // Features
 export { default as useFilesStore } from './features/files/filesStore'
-// export { default as useFoldersStore } from './features/folders/foldersStore'
+export { useFoldersStore } from './features/folders/foldersStore'
 export { default as usePdfEditorStore } from './features/pdf-editor/pdfEditorStore'
-// export { default as useSignaturesStore } from './features/signatures/signaturesStore'
 export { default as useTransferStore } from './features/transfer/transferStore'
 
 // UI
 export { default as useModalStore } from './ui/modalStore'
-// export { default as useSidebarStore } from './ui/sidebarStore'
 
 // Auth
 export { default as useAuthStore } from './auth/authStore'
